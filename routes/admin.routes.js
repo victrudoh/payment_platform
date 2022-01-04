201855c354a74f016fe6c0413bae1257638009d2
Edikan <victrudoh@gmail.com>
2022-01-04 15:30:04 +0100

    admin account updates

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -9,32 +9,16 @@ const {authorize} = require("../middlewares/roleCheck");
 
 const router = express.Router();
 
-// router.get("/login", adminController.loginController);
-
-router.get("/add_product", isAuthenticated, authorize('admin'), adminController.getAddProductController);
-
-router.post("/add_product", isAuthenticated, authorize('admin'), adminController.postAddProductController);
-
-router.get("/products", authorize('admin'), adminController.getProductController);
-
-router.post("/productsSort", authorize('admin'), adminController.postSortProductsController);
-
-router.get("/edit_product/:productId", isAuthenticated, authorize('admin'), adminController.getEditProductController);
-
-router.post("/edit_product", isAuthenticated, authorize('admin'), adminController.postEditProductController);
-
-router.post("/delete_product", isAuthenticated, authorize('admin'), adminController.postDeleteProductController);
-
-router.post("/disable_product", isAuthenticated, authorize('admin'), adminController.toggleDisableProductController
-);
 
 router.get("/dashboard", authorize('admin'), adminController.getDashboardController);
 
-router.get("/orders", authorize('admin'), adminController.getOrdersController);
+router.get("/pendingTransactions", authorize('admin'), adminController.getPendingTransactionsController);
 
-router.get("/ordersPayment", authorize('admin'), adminController.getOrdersPaymentController);
+router.get("/completedTranscations", authorize('admin'), adminController.getCompletedTranscationsController);
 
-router.post("/orders", authorize('admin'), adminController.getOrdersController);
+// router.get("/ordersPayment", authorize('admin'), adminController.getOrdersPaymentController);
+
+router.post("/orders", authorize('admin'), adminController.getPendingTransactionsController);
 
 router.get("/users", authorize("admin"), adminController.getUsersController);
 
@@ -43,10 +27,5 @@ router.get("/edit_user/:id", authorize("admin"), adminController.postEditUserCon
 router.post("/update_user", authorize("admin"), adminController.postUpdateUserController);
 
 router.get("/out_of_stock", authorize("admin"), adminController.getDisabledProductsController);
-
-router.post("/upload", authorize("admin"), adminController.postUploadController);
-
-router.post("/uploadExcel", authorize("admin"), adminController.postUploadExcelController);
-
 
 module.exports = router;
