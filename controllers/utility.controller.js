e862836ac27838f08279532b0d3778ce622df9ef
Edikan <victrudoh@gmail.com>
2022-01-12 15:41:49 +0100

    switched redirect url for flutterwave

--- controllers/utility.controller.js
+++ controllers/utility.controller.js
@@ -140,8 +140,8 @@ module.exports = {
       amount: newAmount,
       currency: currency,
       payment_options: "card",
-      redirect_url: "http://localhost:4000/utility/verify",
-      // redirect_url: "https://topapp-ng.herokuapp.com/utility/verify",
+      // redirect_url: "http://localhost:4000/utility/verify",
+      redirect_url: "https://topapp-ng.herokuapp.com/utility/verify",
       customer: {
         email: req.body.email,
         phonenumber: req.body.phone,
